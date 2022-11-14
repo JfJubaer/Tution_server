7b8da0b72b8c329e47ff9e99bf36c9d7fac10e13
JfJubaer <skk5727@gmail.com>
2022-11-14 13:22:37 +0600

    4th

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.send("24 hours service is on")
+})
+
+app.listen(port,()=>{
+    console.log('Server is running at ',port);
 })
